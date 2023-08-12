f1925d7a06c956a18fad1b93cc6dcb00845fb2de
VegetableSchmedly <iericdaly@gmail.com>
2023-08-11 20:14:49 -0400

    Added Logo. Done for the night.

--- terminal-scheduler/src/components/Navbar.js
+++ terminal-scheduler/src/components/Navbar.js
@@ -1,13 +1,22 @@
 import { Link } from 'react-router-dom';
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap'
+import logo from "../img/logo.png";
 
 const Navigation = () => {
 
     return(
     <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
       <Container fluid>
-        <Navbar.Brand href="">Terminal Gantt</Navbar.Brand>
+          <Navbar.Brand>
+            <img
+              src= {logo}
+              width="75"
+              height="75"
+              className="logo"
+              alt="Terminal Gantt"
+            />
+          </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='me-auto'>
